d87260e5501e0288008812550790e2a520dc4756
Celeste de Groot <celestemakesgames@gmail.com>
2026-09-13 19:54:23 +0200

    Removed template ASP.NET webapi

--- CameraApi/obj/Debug/net10.0/CameraApi.AssemblyInfo.cs
+++ CameraApi/obj/Debug/net10.0/CameraApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CameraApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c29b4518af84caf3bca49f5289c3d6ad3f3dd3d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CameraApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CameraApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
